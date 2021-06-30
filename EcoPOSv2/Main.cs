bf9213e8693df792152045f6707c75af7b6c6b6b
Eloisa Jane Calanao <ejcalanao27@gmail.com>
2021-06-30 16:13:49 +0800

    calculator - ok

--- EcoPOSv2/Main.cs
+++ EcoPOSv2/Main.cs
@@ -403,12 +403,13 @@ namespace EcoPOSv2
         }
         private void btnCalculator_Click(object sender, EventArgs e)
         {
-            if(Order.Instance.CheckOpened("Calculator") == true)
-            {
-                return;
-            }
+            Process.Start("calc.exe");
+            //if(Order.Instance.CheckOpened("Calculator") == true)
+            //{
+            //    return;
+            //}
 
-            new Calculator().Show();
+            //new Calculator().Show();
         }
     }
 }
