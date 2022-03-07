ce847d479529eab772233349b4ced2ac4527054f
DinMrndn <dinearlm@gmail.com>
2022-03-07 15:44:56 +0800

    Adjust Cost Mandarin Language Support

--- EcoPOSv2/AdjustCost.Designer.cs
+++ EcoPOSv2/AdjustCost.Designer.cs
@@ -43,121 +43,88 @@
             // 
             // guna2Panel1
             // 
+            resources.ApplyResources(this.guna2Panel1, "guna2Panel1");
             this.guna2Panel1.Controls.Add(this.btnCancel);
             this.guna2Panel1.Controls.Add(this.btnConfirm);
             this.guna2Panel1.Controls.Add(this.txtRemarks);
             this.guna2Panel1.Controls.Add(this.Label5);
             this.guna2Panel1.Controls.Add(this.Label2);
             this.guna2Panel1.Controls.Add(this.txtNewCost);
-            this.guna2Panel1.Location = new System.Drawing.Point(1, 1);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
-            this.guna2Panel1.Size = new System.Drawing.Size(425, 353);
-            this.guna2Panel1.TabIndex = 0;
             // 
             // btnCancel
             // 
+            resources.ApplyResources(this.btnCancel, "btnCancel");
             this.btnCancel.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.btnCancel.BorderThickness = 2;
             this.btnCancel.CheckedState.Parent = this.btnCancel;
             this.btnCancel.CustomImages.Parent = this.btnCancel;
             this.btnCancel.FillColor = System.Drawing.Color.White;
-            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
             this.btnCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.btnCancel.HoverState.Parent = this.btnCancel;
             this.btnCancel.Image = ((System.Drawing.Image)(resources.GetObject("btnCancel.Image")));
             this.btnCancel.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.btnCancel.ImageOffset = new System.Drawing.Point(10, 0);
             this.btnCancel.ImageSize = new System.Drawing.Size(40, 40);
-            this.btnCancel.Location = new System.Drawing.Point(224, 285);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.ShadowDecoration.Parent = this.btnCancel;
-            this.btnCancel.Size = new System.Drawing.Size(189, 57);
-            this.btnCancel.TabIndex = 56;
-            this.btnCancel.Text = "CANCEL";
             this.btnCancel.TextOffset = new System.Drawing.Point(10, 0);
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnConfirm
             // 
+            resources.ApplyResources(this.btnConfirm, "btnConfirm");
             this.btnConfirm.BorderColor = System.Drawing.Color.Green;
             this.btnConfirm.BorderThickness = 2;
             this.btnConfirm.CheckedState.Parent = this.btnConfirm;
             this.btnConfirm.CustomImages.Parent = this.btnConfirm;
             this.btnConfirm.FillColor = System.Drawing.Color.White;
-            this.btnConfirm.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
             this.btnConfirm.ForeColor = System.Drawing.Color.Green;
             this.btnConfirm.HoverState.Parent = this.btnConfirm;
             this.btnConfirm.Image = ((System.Drawing.Image)(resources.GetObject("btnConfirm.Image")));
             this.btnConfirm.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.btnConfirm.ImageOffset = new System.Drawing.Point(10, 0);
             this.btnConfirm.ImageSize = new System.Drawing.Size(40, 40);
-            this.btnConfirm.Location = new System.Drawing.Point(11, 285);
             this.btnConfirm.Name = "btnConfirm";
             this.btnConfirm.ShadowDecoration.Parent = this.btnConfirm;
-            this.btnConfirm.Size = new System.Drawing.Size(189, 57);
-            this.btnConfirm.TabIndex = 55;
-            this.btnConfirm.Text = "CONFIRM";
             this.btnConfirm.TextOffset = new System.Drawing.Point(10, 0);
             this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
             // txtRemarks
             // 
-            this.txtRemarks.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.txtRemarks, "txtRemarks");
             this.txtRemarks.ForeColor = System.Drawing.Color.Black;
-            this.txtRemarks.Location = new System.Drawing.Point(11, 133);
-            this.txtRemarks.Multiline = true;
             this.txtRemarks.Name = "txtRemarks";
-            this.txtRemarks.Size = new System.Drawing.Size(402, 135);
-            this.txtRemarks.TabIndex = 46;
             this.txtRemarks.Tag = "";
             // 
             // Label5
             // 
-            this.Label5.AutoSize = true;
-            this.Label5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.Label5, "Label5");
             this.Label5.ForeColor = System.Drawing.Color.Black;
-            this.Label5.Location = new System.Drawing.Point(11, 99);
             this.Label5.Name = "Label5";
-            this.Label5.Size = new System.Drawing.Size(85, 28);
-            this.Label5.TabIndex = 47;
-            this.Label5.Text = "Remarks";
-            this.Label5.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             // 
             // Label2
             // 
-            this.Label2.AutoSize = true;
-            this.Label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.Label2, "Label2");
             this.Label2.ForeColor = System.Drawing.Color.Black;
-            this.Label2.Location = new System.Drawing.Point(11, 16);
             this.Label2.Name = "Label2";
-            this.Label2.Size = new System.Drawing.Size(92, 28);
-            this.Label2.TabIndex = 45;
-            this.Label2.Text = "New cost";
             // 
             // txtNewCost
             // 
-            this.txtNewCost.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.txtNewCost, "txtNewCost");
             this.txtNewCost.ForeColor = System.Drawing.Color.Black;
-            this.txtNewCost.Location = new System.Drawing.Point(11, 52);
-            this.txtNewCost.Multiline = true;
             this.txtNewCost.Name = "txtNewCost";
-            this.txtNewCost.Size = new System.Drawing.Size(402, 33);
-            this.txtNewCost.TabIndex = 42;
             this.txtNewCost.Tag = "";
             // 
             // AdjustCost
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(427, 355);
             this.Controls.Add(this.guna2Panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AdjustCost";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "AdjustCost";
             this.Load += new System.EventHandler(this.AdjustCost_Load);
             this.guna2Panel1.ResumeLayout(false);
             this.guna2Panel1.PerformLayout();
